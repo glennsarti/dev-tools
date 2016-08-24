7c4069949302ddedacba3c987b2b9ed95fe3fb69
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-08-24 13:46:02 -0700

    Made pooler command async

--- pooler.bat
+++ pooler.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-powershell "& { Import-Module 'C:\Source\posh-vmpool\src\functions\module.psm1'; Start-VMPoolerUI -URL 'https://vmpooler.delivery.puppetlabs.net/api/v1' -Verbose }"
+start powershell "& { Import-Module 'C:\Source\posh-vmpool\src\functions\module.psm1'; Start-VMPoolerUI -URL 'https://vmpooler.delivery.puppetlabs.net/api/v1' -Verbose }"
