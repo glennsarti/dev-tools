6d1a72a862279a437479b6d45f0703707d050562
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-09-16 14:35:46 -0700

    Updated and now works!

--- Install-cpp-build.ps1
+++ Install-cpp-build.ps1
@@ -35,7 +35,7 @@ if ($rubyExe -eq $null) {
   $misc = Read-Host -Prompt "Would you like to install Ruby? (Y/N)"
   if (($misc -eq 'Y') -or ($misc -eq 'y')) {
     Write-Host 'Starting the installation'
-    & choco install ruby -yee
+    & choco install ruby -y
     Write-Host "-------------------------------"
     Write-Host "You will need to shut down and restart powershell and/or consoles first prior to using ruby."
     return;
@@ -73,16 +73,36 @@ Write-Verbose "Installing chocolatey packages..."
 
 # Modify the PATH
 Write-Verbose "Modifying the PATH..."
-if (-not ($ENV:Path -like '*C:\tools\mingw64\bin*')) {
-  Write-Verbose "Adding mingw64 to the path..."
-  $ENV:PATH = 'C:\tools\mingw64\bin;' + $ENV:PATH  
-}
 if (-not ($ENV:Path -like '*C:\Program Files\gettext-iconv*')) {
   Write-Verbose "Adding gettext-iconv to the path..."
   $ENV:PATH = 'C:\Program Files\gettext-iconv;' + $ENV:PATH  
 }
+# MingW _MUST_ be at the beginning of the path
+if (-not ($ENV:Path -like 'C:\tools\mingw64\bin*')) {
+  Write-Verbose "Adding mingw64 to the path..."
+  $ENV:PATH = 'C:\tools\mingw64\bin;' + $ENV:PATH  
+}
 $ENV:PATH = $ENV:PATH.Replace("Git\bin", "Git\cmd")
 $ENV:PATH = $ENV:PATH.Replace("Git\usr\bin", "Git\cmd")
+
+# Modify SYSTEM PATH if needed
+$misc = Read-Host -Prompt "Should the PATH environment variable for the computer be modified too? (Y/N)"
+if (($misc -eq 'Y') -or ($misc -eq 'y')) {
+  Write-Warning "**** THIS MAY REQUIRE A LOGOFF OR REBOOT TO TAKE AFFECT ****"
+  $Hive = [Microsoft.Win32.Registry]::LocalMachine
+  $Key = $Hive.OpenSubKey("SYSTEM\CurrentControlSet\Control\Session Manager\Environment",$true)
+
+  $CurrentPATH = $Key.GetValue("path",$False, [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames)
+  if (-not ($CurrentPATH -like '*C:\Program Files\gettext-iconv*')) {
+    $CurrentPATH = 'C:\Program Files\gettext-iconv;' + $CurrentPATH
+  }
+  # MingW _MUST_ be at the beginning of the path
+  if (-not ($CurrentPATH -like 'C:\tools\mingw64\bin*')) {
+    $CurrentPATH = 'C:\tools\mingw64\bin;' + $CurrentPATH
+  }
+  
+  $Key.SetValue('Path',$CurrentPATH,'ExpandString')
+}
 
 # Temp file for downloads
 $tempFile = Join-Path -Path $ENV:Temp -ChildPath 'cpp-temp-download.7z'
@@ -167,10 +187,21 @@ Write-Verbose "Create build batch files..."
 
 SET PATH=$($ENV:PATH)
 
-cmake -G `"MinGW Makefiles`" -DBOOST_ROOT=`"$($boostDir.FullName)`" -DYAMLCPP_ROOT=`"$($yamlcppDir.FullName)`" -DBOOST_STATIC=ON -DCURL_STATIC=ON -DCMAKE_INSTALL_PREFIX=`"C:\Program Files\FACTER`" -DCMAKE_PREFIX_PATH=`"$($leathermanDir.FullName);$($curlDir.FullName);$($hoconDir.FullName)`" .
+SET FACTER_SRC=.
+if NOT [%1]==[] SET FACTER_SRC=%1
+
+cmake -G `"MinGW Makefiles`" -DBOOST_ROOT=`"$($boostDir.FullName)`" -DYAMLCPP_ROOT=`"$($yamlcppDir.FullName)`" -DBOOST_STATIC=ON -DCURL_STATIC=ON -DCMAKE_INSTALL_PREFIX=`"C:\Program Files\FACTER`" -DCMAKE_PREFIX_PATH=`"$($leathermanDir.FullName);$($curlDir.FullName);$($hoconDir.FullName)`" %FACTER_SRC%
 mingw32-make clean install -j2
 
 "@ | Out-File -FilePath "$INSTALL_LOCATION\build-facter.bat" -Encoding 'ASCII'
+
+@"
+@ECHO OFF
+
+SET PATH=$($ENV:PATH)
+
+ECHO Ready for work
+"@ | Out-File -FilePath "$INSTALL_LOCATION\build-env.bat" -Encoding 'ASCII'
 
 Write-Verbose "Create test ps1 files..."
 @"
@@ -185,9 +216,12 @@ mingw32-make install
 -----------------------------------------------------------------------
 From within a puppetlabs-facter repository you can call:
 
-$($INSTALL_LOCATION)\build-facter.bat     Builds facter
+$($INSTALL_LOCATION)\build-env.bat           Sets environment vars for building facter
 
-$($INSTALL_LOCATION)\test-facter.bat      Runs tests
+$($INSTALL_LOCATION)\build-facter.bat        Builds facter from the root of a facter repo
+$($INSTALL_LOCATION)\build-facter.bat <path> Builds facter with a path to the facter source e.g. in 'facter/release' would call '$($INSTALL_LOCATION)\build-facter.bat ..'
+
+$($INSTALL_LOCATION)\test-facter.bat         Runs tests from the root of a facter repo
 
 -----------------------------------------------------------------------
 "@ | Write-Host -ForegroundColor Green
