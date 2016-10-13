a50d1380557c60538e92c43d0855fad63c6621de
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-10-12 17:40:38 -0700

    Update gem update
    The file 'gem' is a actually a bat file so you must use CALL when calling a BAT file from within a BAT file.

--- update-system-gems.bat
+++ update-system-gems.bat
@@ -1,8 +1,14 @@
 @ECHO OFF
 
 ECHO Updating system gems (via HTTP)
-gem sources -r https://rubygems.org/
-gem sources -a http://rubygems.org/
-gem update --system
-gem sources -r http://rubygems.org/
-gem sources -a https://rubygems.org/
+
+ECHO Using http for gem sources ...
+call gem sources -r https://rubygems.org/
+call gem sources -a http://rubygems.org/
+
+ECHO Updating...
+call gem update --system
+
+ECHO Restoring gem sources...
+call gem sources -r http://rubygems.org/
+call gem sources -a https://rubygems.org/
