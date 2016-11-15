d4c388dcb7c9fa1eba74ca4dc59d5bb88a783d56
Glenn Sarti <glennsarti@users.noreply.github.com>
2016-11-15 13:11:40 -0800

    Update pe-clienttools config script
    Updated the pe client tools config script;
    - Takes and argument called -PuppetMaster to assist in automation
    - Uses .Net WebClient class instead of Invoke-WebRequest in order to support
      Powershell versions less than 3 e.g. Windows 7

--- config-pe-client-tools.ps1
+++ config-pe-client-tools.ps1
@@ -1,9 +1,14 @@
+[CmdletBinding()]
+param(
+  [Parameter(Mandatory = $false)]
+  [String]$PuppetMaster = ''
+)
 $ErrorActionPreference = 'Stop'
 
 $userDir = Join-Path -Path $ENV:USERPROFILE -ChildPath '.puppetlabs'
 $userClientToolsDir = Join-Path -Path $userDir -ChildPath 'client-tools'
 $tokenFile = Join-Path -Path $userDir -ChildPath 'token'
-$certsDir = 'C:\ProgramData\PuppetLabs\puppet\etc\ssl\certs'
+$certsDir = Join-Path -Path $ENV:ALLUSERSPROFILE -ChildPath 'PuppetLabs\puppet\etc\ssl\certs'
 
 ## DANGER - Major hack
 Add-type @"
@@ -48,7 +53,7 @@ Function Invoke-ShowConfiguration() {
 
 }
 
-Function Invoke-QuickConfig() {
+Function Invoke-QuickConfig($puppetMaster = '') {
   Write-Host "Creating required directories..."
   # Quick hack but it works ...
   if (-Not (Test-Path -Path $certsDir)) {
@@ -58,7 +63,9 @@ Function Invoke-QuickConfig() {
     (& cmd /c md "`"$userClientToolsDir`"") | Out-Null
   }
 
-  $puppetMaster = Read-Host "Enter Puppet Master name"
+  if ($puppetMaster -eq '') {
+    $puppetMaster = Read-Host "Enter Puppet Master name"
+  }
  
   # Sanity Check - Resolve by name
   try {
@@ -76,12 +83,23 @@ Function Invoke-QuickConfig() {
     Write-Host "Removing previous CA Master certificate..."
     Remove-Item -Path $caCertFile -Force -Confirm:$false | Out-Null
   }
-  [System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy
-  Write-Host "Fetching the CA Master certificate ..."
-  $response = Invoke-WebRequest -URI "https://$($puppetMaster):8140/puppet-ca/v1/certificate/ca" -UseBasicParsing
-  if ($response.StatusCode -ne 200) { Write-Warning "Response code $($response.StatusCode) from Puppet Master was not OK."; return }
-  $response.Content | Out-File -FilePath $caCertFile -Encoding "ASCII"
-  
+
+  try {
+    [System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy
+    Write-Host "Fetching the CA Master certificate ..."
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile("https://$($puppetMaster):8140/puppet-ca/v1/certificate/ca",$caCertFile)
+  } catch {
+    Write-Warning "Error from $PuppetMaster was $_"
+    return
+  }
+
+  # [System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy
+  # Write-Host "Fetching the CA Master certificate ..."
+  # $response = Invoke-WebRequest -URI "https://$($puppetMaster):8140/puppet-ca/v1/certificate/ca" -UseBasicParsing
+  # if ($response.StatusCode -ne 200) { Write-Warning "Response code $($response.StatusCode) from Puppet Master was not OK."; return }
+  # $response.Content | Out-File -FilePath $caCertFile -Encoding "ASCII"
+
   Write-Host "Writing config files with defaults..."
   # Write the config files
 @"
@@ -129,19 +147,24 @@ If (-not (Test-Path -Path $PEClientToolsBinPath)) {
   return 
 }
 
-$validOptions = @('1','2','3')
+if ($PuppetMaster -eq '') {
+  $validOptions = @('1','2','3')
 
-# Show Menu
-Write-Host '1. Start the PE Client Tools Shell'
-Write-Host '2. Show current PE Client Tool configuration'
-Write-Host '3. Quick config PE Client Tools'
-Write-Host ''
-do {
-  $option = Read-Host -Prompt "Enter select ($($validOptions -join ','))"
-} until ($validOptions -contains $option)
+  # Show Menu
+  Write-Host '1. Start the PE Client Tools Shell'
+  Write-Host '2. Show current PE Client Tool configuration'
+  Write-Host '3. Quick config PE Client Tools'
+  Write-Host ''
+  do {
+    $option = Read-Host -Prompt "Enter select ($($validOptions -join ','))"
+  } until ($validOptions -contains $option)
 
-switch ($option) {
-  '1' { Start-Process -FilePath "cmd.exe" -Argument @('/k',"`"$($PEClientToolsBinPath)\pe_client_shell.bat`"") | Out-Null }
-  '2' { Invoke-ShowConfiguration }
-  '3' { Invoke-QuickConfig }
+  switch ($option) {
+    '1' { Start-Process -FilePath "cmd.exe" -Argument @('/k',"`"$($PEClientToolsBinPath)\pe_client_shell.bat`"") | Out-Null }
+    '2' { Invoke-ShowConfiguration }
+    '3' { Invoke-QuickConfig }
+  }
+} else {
+  Write-Host "Running quick config for puppet master $PuppetMaster"
+  Invoke-QuickConfig -PuppetMaster $PuppetMaster
 }
