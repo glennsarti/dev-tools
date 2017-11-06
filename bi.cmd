95500801d11425eece796c060b02950c446ec1ea
Glenn Sarti <glennsarti@users.noreply.github.com>
2017-11-06 14:47:53 -0800

    Update BundleInstall

--- bi.cmd
+++ bi.cmd
@@ -1,5 +1,11 @@
 @ECHO OFF
 
 RD .bundle\windows /s/q
+DEL .bundle\config /q /f
 del Gemfile.lock
-bundle install --path .bundle\windows --without system_tests %*
+
+if [%1]==[] (
+  CALL bundle install --path .bundle\windows --without system_tests %*
+) ELSE (
+  CALL bundle install --path .bundle\windows %*
+)
--- bi.sh
+++ bi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf .bundle/bash
+rm .bundle/config
 rm Gemfile.lock
 
 bundle install --path .bundle/bash --with system_tests  "$@"
