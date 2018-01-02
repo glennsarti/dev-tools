f9cea24f3639e367a9363f0c564240bf87cf935c
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-01-02 08:56:00 +0800

    Update scpsync to prep a windows target

--- scpsync.ps1
+++ scpsync.ps1
@@ -1,6 +1,7 @@
 param(
   $target = '',
-  $destDir = ''
+  $destDir = '',
+  [Switch]$PrepTarget
 )
 $ErrorActionPreference = 'Stop'
 
@@ -8,6 +9,20 @@ $ErrorActionPreference = 'Stop'
 
 $thisDir = Get-Location
 if ($destDir -eq '') { $destDir = $thisDir }
+
+if ($PrepTarget) {
+  Write-Host "Preparing Target machine..."
+
+  Invoke-Command -ComputerName $target -Credential (Get-Credential 'Administrator') -ArgumentList @($destDir) -ScriptBlock {
+    param($destDir)
+    if (-Not (Test-Path -Path $destDir)) {
+      Write-Host "Creating destination dir..."
+      New-Item $destDir -ItemType Directory | Out-Null
+    }
+
+    & choco install openssh -y -params '"/SSHServerFeature"'
+  }
+}
 
 Write-Host "Connecting to $target ..."
 & "C:\Program Files (x86)\WinSCP\WinSCP.com" /ini=nul /script=C:\Source\dev-tools\scpsync.script /parameter $target ($thisDir -replace '\\','/') ($destDir -replace '\\','/')
