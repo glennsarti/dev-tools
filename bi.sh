8dc6d07d8abde0985b2ab5e1e254e5cf10d86360
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-05-10 11:02:02 +0800

    Add args support for bi.sh

--- bi.sh
+++ bi.sh
@@ -4,4 +4,8 @@ rm -rf .bundle/bash
 rm .bundle/config
 rm Gemfile.lock
 
-bundle install --path .bundle/bash --with system_tests  "$@"
+if [ -z $1 ]; then
+  bundle install --path .bundle/bash --with system_tests "$@"
+else
+  bundle install --path .bundle/bash "$@"
+fi
