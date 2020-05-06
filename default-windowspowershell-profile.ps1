9eede06f2b8705ef546ee948a6fd91ac6de034c1
Glenn Sarti <glennsarti@users.noreply.github.com>
2020-05-06 10:23:16 +0800

    (maint) Update PS Profile

--- default-windowspowershell-profile.ps1
+++ default-windowspowershell-profile.ps1
@@ -1,17 +1,20 @@
 param([Switch]$Install)
 
 if ($Install) {
-  $ProfileFile = Join-Path -Path ([environment]::getfolderpath("mydocuments")) -ChildPath 'WindowsPowerShell\Profile.ps1'
+  $ProfileFile = $PROFILE # Join-Path -Path ([environment]::getfolderpath("mydocuments")) -ChildPath 'WindowsPowerShell\Profile.ps1'
 
   if (-not (Test-Path -Path $ProfileFile)) {
     ". `"$PSCommandPath`"" | Set-Content $ProfileFile -Encoding UTF8 -Force -Confirm:$false
   } else {
     "`n. `"$PSCommandPath`"" | Out-File $ProfileFile -Encoding UTF8 -Append -Force -Confirm:$false
   }
+  return
 }
 
 If (($null -eq $ENV:ConEmuHWND) -and ($ENV:TERM_PROGRAM -ne 'vscode')) {
-  Import-Module PSConsoleTheme
-  Set-ConsoleTheme 'Bright'
+  # Import-Module PSConsoleTheme
+  # Set-ConsoleTheme 'Bright'
   Set-Location C:\Source
 }
+
+Import-Module Posh-Git
